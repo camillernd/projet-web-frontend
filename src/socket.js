da96302397477be954c8936b553227f43f4ddda2
Camille Renard <camille.renard@etu.umontpellier.fr>
2024-05-23 19:26:02 +0200

    ajout https

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://aftermovie-backend.cluster-ig3.igpolytech.fr');
+const socket = io('https://aftermovie-backend.cluster-ig3.igpolytech.fr');
 
 export default socket;
 
