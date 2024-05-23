09bf21116b8dd79204b85fe7089dda5e27aa0bc4
Camille Renard <camille.renard@etu.umontpellier.fr>
2024-05-23 19:39:14 +0200

    ajout port 5000

--- src/components/socket.js
+++ src/components/socket.js
@@ -1,6 +1,6 @@
 // socket.js
 import { io } from 'socket.io-client';
 
-const socket = io('https://aftermovie-backend.cluster-ig3.igpolytech.fr');
+const socket = io('https://aftermovie-backend.cluster-ig3.igpolytech.fr:5000');
 
 export default socket;
