2b24efa81736811c553de3707dae36805abd7a1a
Camille Renard <camille.renard@etu.umontpellier.fr>
2024-05-27 20:07:10 +0200

    correction like

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,45 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { IconButton, Tooltip } from '@mui/material';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import heartFullIcon from '../assets/full-heart.png';
+import heartEmptyIcon from '../assets/empty-heart.png';
 import './LikeButton.css';
 
 function LikeButton({ user, messageId, socket, likesCount }) {
   const [liked, setLiked] = useState(false);
+  const [currentLikesCount, setCurrentLikesCount] = useState(likesCount);
 
   useEffect(() => {
-    if (user) {
-      const fetchLikeStatus = async () => {
-        try {
-          const response = await axios.get(`http://localhost:3000/api/like/check?messageId=${messageId}&userId=${user.userId}`);
-          setLiked(response.data.liked);
-        } catch (error) {
-          console.error('Error fetching like status:', error);
-        }
-      };
+    const fetchLikeStatus = async () => {
+      try {
+        const response = await axios.get(`http://aftermovie-backend.cluster-ig3.igpolytech.fr/api/like/check?messageId=${messageId}&userId=${user.userId}`);
+        setLiked(response.data.liked);
+      } catch (error) {
+        console.error('Error fetching like status:', error);
+      }
+    };
 
-      fetchLikeStatus();
-    }
-  }, [messageId, user]);
+    fetchLikeStatus();
+  }, [messageId, user.userId]);
 
   const handleLike = async () => {
-    if (!user) return;
-
     try {
       if (liked) {
-        await axios.delete('http://localhost:3000/api/like', {
+        await axios.delete('http://aftermovie-backend.cluster-ig3.igpolytech.fr/api/like', {
           data: {
             messageId: messageId,
             userId: user.userId
           }
         });
         setLiked(false);
+        setCurrentLikesCount(currentLikesCount - 1);
         socket.emit('unlikeMessage', { messageId: messageId, userId: user.userId });
       } else {
-        await axios.post(`http://localhost:3000/api/like`, { messageId, userId: user.userId });
+        await axios.post(`http://aftermovie-backend.cluster-ig3.igpolytech.fr/api/like`, { messageId, userId: user.userId });
         setLiked(true);
+        setCurrentLikesCount(currentLikesCount + 1);
         socket.emit('likeMessage', { messageId: messageId, userId: user.userId });
+        
       }
     } catch (error) {
       console.error('Error updating like status:', error);
@@ -47,16 +46,10 @@ function LikeButton({ user, messageId, socket, likesCount }) {
   };
 
   return (
-    <div className="like-button">
-      <Tooltip title={liked ? "Unlike" : "Like"}>
-        <span>
-          <IconButton onClick={handleLike} disabled={!user}>
-            {liked ? <FavoriteIcon color="error" /> : <FavoriteBorderIcon />}
-          </IconButton>
-        </span>
-      </Tooltip>
-      <span className="likes-count">{likesCount}</span>
-    </div>
+    <button onClick={handleLike} className="like-button">
+      <img src={liked ? heartFullIcon : heartEmptyIcon} alt="Like" className="like-icon" />
+      <span className="likes-count">{currentLikesCount}</span>
+    </button>
   );
 }
 
